8fcf74636c98fa307025e6f8d90fb698a10732e5
Emil Basnyat <emilbasnyat@Emils-MacBook-Pro.local>
2024-12-09 11:01:56 +0545

    homepage with products

---
+++ src/components/LatestCollection.jsx
@@ -0,0 +1,53 @@
+import { useContext, useEffect, useState } from "react"
+import { ShopContext } from "../context/ShopContext"
+import Title from "./Title.jsx";
+import ProductItem from "./ProductItem.jsx";
+
+const LatestCollection = () => {
+  const { products } = useContext(ShopContext);
+    const[latestproducts, setLatestProducts] = useState([]);
+
+    useEffect(()=>{
+        setLatestProducts(products.slice(0,10));
+    },[])
+
+  return (
+    <div className="my-10">
+      <div className="text-center py-8 text-3x1">
+        <Title text1={'LATEST'} text2={'COLLECTIONS'} />
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
+        New products of pose fashion add any text from components latestproducts
+        </p>
+      </div>
+
+        {/* RENDERING PRODUCTS  */}
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+
+            {
+                latestproducts.map((item,index)=>(
+                    <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price}/>
+                ))
+            }
+            </div>
+    </div>
+  )
+}
+
+export default LatestCollection
+
+
+
+// import React, { useContext } from 'react'
+// import { ShopContext } from '../context/ShopContext';
+
+// const LatestCollection = () => {
+//     const { products } = useContext(ShopContext);
+
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+
+// export default LatestCollection
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import { useState } from 'react'
 import { assets } from '../assets/assets'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
+    const [visible, setVisible] = useState(false);
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
 
@@ -44,9 +45,40 @@ const Navbar = () => {
                 <p className='cursor-pointer hover:text-black'>Orders</p>
                 <p className='cursor-pointer hover:text-black'>Logout</p>
             </div>
-            </div>     
+            </div> 
+                
         </div>
+        {/* <Link to = '/cart' className='relative'>
+            <img src={assets.cart_icon} className='w-5 min-w-5' alt=""/>
+            <p className='absolute right- [ -5px ] bottom- [-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
+            
+            
+
+        </Link> */}
+
+        <Link to="/cart" className="relative inline-block">
+  <img src={assets.cart_icon} className="w-5 min-w-5" alt="Cart Icon" />
+  <p className="absolute right-[-5px] bottom-[-5px] w-4 h-4 text-center leading-[16px] bg-black text-white rounded-full text-[8px]">
+    10
+  </p>
+</Link>
+        <img onClick={()=>setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt=''/>
         </div>
+        {/* Sidebar for smallscreens */}
+        <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full':'w-0'}`}>
+            <div className='flex flex-col text-grey-600'>
+                <div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
+                    <img className='h-4 rotate-180' src={assets.dropdown_icon} alt=''/>
+                    <p>Back</p>
+
+                </div>
+                <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/'>HOME</NavLink>
+                <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/collection'>COLLECTION</NavLink>
+                <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/about'>ABOUT</NavLink>
+                <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/contact'>CONTACT</NavLink>
+            </div>
+        </div>
+
 
     </div>
   )
