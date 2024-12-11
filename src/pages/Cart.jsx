f075370728001b8b79846799049b60e87928b6bf
Emil Basnyat <emilbasnyat@Emils-MacBook-Pro.local>
2024-12-11 16:47:58 +0545

    delivary information section

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,11 +3,13 @@ import { ShopContext } from '../context/ShopContext'
 import Title from '../components/Title';
 import { assets } from '../assets/assets';
 import CartTotal from '../components/CartTotal';
+import { useNavigate } from 'react-router-dom';
 
 const Cart = () => {
   
   const { products, currency, cartItems, updateQuantity} = useContext(ShopContext);
   const [cartData, setCartData] = useState([]);
+  const navigate = useNavigate()
   
   useEffect(()=>{
     const tempData = [];
@@ -60,6 +62,9 @@ const Cart = () => {
       <div className='flex justify-end my-20'>
         <div className='w-full sm:w-[450px]'>
           <CartTotal/>
+          <div className='w-full text-end'>
+            <button onClick={()=>navigate('/place-order')} className='bg-black text-white text-sm my-8 px-8 py-3'>PROCEED TO PAYMENT</button>
+          </div>
         
         </div> 
 
